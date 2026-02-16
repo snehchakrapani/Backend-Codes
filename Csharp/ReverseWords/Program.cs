55942fed003d9a53c7d5814cc05952e97a8deb86
snehchakrapani <snehchakrapani2002@gmail.com>
2026-02-16 12:20:32 +0530

    Reverse Words code

---
+++ Csharp/ReverseWords/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string sentence = "My organization name is InTimeTEC";
+
+        sentence = sentence.ToLower();
+
+        string[] words = sentence.Split(' ');
+
+        Console.WriteLine("The total word count in the given sentence is: " + words.Length);
+
+        Console.Write("The reversed order of the given sentence : ");
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            string word = words[i];
+
+            for (int j = word.Length - 1; j >= 0; j--)
+            {
+                Console.Write(word[j]);
+            }
+
+            if (i != words.Length - 1)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        Console.Write(".");
+    }
+}
