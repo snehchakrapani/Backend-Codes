b8d8cd036844de49faf38e3c7cbca0f4bac425d5
snehchakrapani <snehchakrapani2002@gmail.com>
2026-02-16 12:05:03 +0530

    Problen 2 number pyramid

---
+++ Csharp/Number Pyramid/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main()
+    {
+        int totalRows = 5;
+
+  
+        for (int i = totalRows; i >= 1; i--)
+        {
+            
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j + " ");
+            }
+
+       
+            Console.WriteLine();
+        }
+    }
+}
