85d341e6bd7109f58ba88312615785e12612ed76
snehchakrapani <snehchakrapani2002@gmail.com>
2026-02-17 09:42:06 +0530

    Week 1 Deliverbale Simple Calculator Program

---
+++ Csharp/Calculator/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Csharp
+{
+    class SimpleCalculator
+    {
+        static void Main()
+        {
+      
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();  
+
+         
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("   SIMPLE CALCULATOR APP        ");
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine();
+
+     
+            Console.Write("Enter first number  : ");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number : ");
+            int number2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter operator      : ");
+            char operation = char.Parse(Console.ReadLine());
+
+          
+            Console.WriteLine("----------------------------");
+            switch (operation)
+            {
+                case '+':
+                  
+                    Console.WriteLine($"  {number1} + {number2} = {number1 + number2}");
+                    break;
+
+                case '-':
+                   
+                    Console.WriteLine($"  {number1} - {number2} = {number1 - number2}");
+                    break;
+
+                case '*':
+                   
+                    Console.WriteLine($"  {number1} × {number2} = {number1 * number2}");
+                    break;
+
+                case '/':
+                    if (number2 == 0)
+                    {
+                      
+                        Console.WriteLine("  ✗ Error: Cannot divide by zero!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"  {number1} ÷ {number2} = {number1 / number2}");
+                    }
+                    break;
+
+                case '%':
+                    if (number2 == 0)
+                    {
+                       
+                        Console.WriteLine("  ✗ Error: Cannot modulus by zero!");
+                    }
+                    else
+                    {
+                      
+                        Console.WriteLine($"  {number1} % {number2} = {number1 % number2}");
+                    }
+                    break;
+
+                default:
+                    
+                    Console.WriteLine("Invalid operator! Use +, -, *, /, or %");
+                    break;
+            }
+
+       
+ 
+            Console.WriteLine("---------------------------");
+            Console.WriteLine();
+            Console.Write("Press any key to exit...");
+
+            Console.ReadKey();
+        }
+    }
+}
