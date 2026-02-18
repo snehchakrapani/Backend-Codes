4599db3e7ea33f353a02d89f4e787f7cc7ae8a97
snehchakrapani <snehchakrapani2002@gmail.com>
2026-02-18 16:06:39 +0530

    program to understand recursion

---
+++ Csharp/RecursionExp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 5;
+        int result = Sum(number);
+        Console.WriteLine($"Sum of 1 to {number} is {result}");
+        Console.ReadKey();
+    }
+
+    static int Sum(int n)
+    {
+        if (n > 0)
+        {
+            return Sum(n - 1) + n; 
+        }
+        return 0;  
+    }
+}
+/*  static int Factorial(int n)
+    {
+        if (n == 1)   Base case
+        {
+            return 1;
+        }
+        else
+        {
+            return n * Factorial(n - 1);   Recursive case
+        }
+    }*/
