f873fae83bce5322a5f2defaceac2e439e0575fb
snehchakrapani <snehchakrapani2002@gmail.com>
2026-02-25 16:48:59 +0530

    made another class for software licence , inherited fun/properties from parent and added new fun and requirements in it

--- Csharp/AssetManagementSystem/Models/SoftwareLicence.cs
+++ Csharp/AssetManagementSystem/Models/SoftwareLicence.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace AssetManagementSystem.Models
 {
-    internal class SoftwareLicence
+    public class SoftwareLicence : Asset
     {
+        private string _licenseKey;
+        private DateTime _expiryDate;
+
+        public string LicenseKey
+        {
+            get { return _licenseKey; } set { _licenseKey = value; 
+        }
+
+
+
     }
 }
