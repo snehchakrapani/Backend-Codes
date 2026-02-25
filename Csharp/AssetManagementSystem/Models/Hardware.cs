1d94a08b88039221ed3ae460ab5e5e3ca4f3c89f
snehchakrapani <snehchakrapani2002@gmail.com>
2026-02-25 17:12:16 +0530

    Made another class for hardware including all info needed for it

--- Csharp/AssetManagementSystem/Models/Hardware.cs
+++ Csharp/AssetManagementSystem/Models/Hardware.cs
@@ -4,7 +4,30 @@ using System.Text;
 
 namespace AssetManagementSystem.Models
 {
-    internal class Hardware
+    public class Hardware : Asset
     {
+        private string _manufacturer;
+        private string _warrantyperiod;
+
+        public string Manufacturer
+        {
+            get { return _manufacturer; }
+            set { _manufacturer = value; }  
+        }
+
+        public string Warranty
+        {
+            get { return _warrantyperiod;}
+        }
+
+        public Hardware(int serialnumber,string name, DateTime dateOfAcquisition,
+            string manufacturer, string warrantyperiod) : base(serialnumber, name, dateOfAcquisition)
+        {
+            _manufacturer = manufacturer;
+            _warrantyperiod = warrantyperiod;
+        }
+
+        public override string 
+
     }
 }
