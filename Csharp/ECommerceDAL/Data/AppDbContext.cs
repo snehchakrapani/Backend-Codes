299babd7843558e704d5e9cdd040d2b444905ab5
snehchakrapani <snehchakrapani2002@gmail.com>
2026-03-18 16:36:42 +0530

    Added EF migrations - created DB

--- Csharp/ECommerceDAL/Data/AppDbContext.cs
+++ Csharp/ECommerceDAL/Data/AppDbContext.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using ECommerceDAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Text;
 
 namespace ECommerceDAL.Data
 {
-    internal class AppDbContext
+    public class AppDbContext : DbContext
     {
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(
+                "Server=localhost;" +
+                "Database=ECommerceDB;" +
+                "Trusted_Connection=True;" +
+                "TrustServerCertificate=True;"
+            );
+        }
     }
 }
